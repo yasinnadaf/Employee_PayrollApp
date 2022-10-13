d3596bd795e1fbee9dbef76c35e14ee6ade8296b
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-10-13 21:50:55 +0530

    set event listner when document is loaded

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,17 +1,8 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-
-    /* UC8 set event listener on salary range*/
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    salary.addEventListener('input', function () {
-        output.textContent = salary.value;
-    });
-});
-
+//UC2_day44
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
+    name.addEventListener('input', function () {
         if (name.value.length == 0) {
             textError.textContent = "";
             return;
@@ -22,5 +13,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
         } catch (e) {
             textError.textContent = e;
         }
+    });
+    /* UC8 set event listener on salary range*/
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector('.salary-output');
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
     });
 });
